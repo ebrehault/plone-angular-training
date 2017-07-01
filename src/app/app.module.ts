0e6a0a19a468a7c1f3d01b64a455f6a7792775ef
Eric BREHAULT <ebrehault@gmail.com>
2017-07-01 19:06:39 +0200

    connect to Plone backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RESTAPIModule } from '@plone/restapi-angular';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +9,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RESTAPIModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'CONFIGURATION', useValue: {
+        BACKEND_URL: 'http://plonerestapi.herokuapp.com/Plone',
+      }
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
